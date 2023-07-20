987946edee984922e39f26b4ee2de2031607fd4a
Emmanuel Okoro <emmaokoro18@gmail.com>
2023-07-20 00:43:52 -0500

    Edited Roboshop project scripts Updated DNS...

--- Roboshop Project/Frontend.sh
+++ Roboshop Project/Frontend.sh
@@ -2,7 +2,7 @@ yum install nginx -y
 
 
 rm -rf /usr/share/nginx/html/*
-cd /usr/share/nginx/html
+#cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 cp Frontend.service /etc/nginx/default.d/roboshop.conf
