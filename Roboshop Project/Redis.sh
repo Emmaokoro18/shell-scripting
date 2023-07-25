a4a7fb880fa4b8df59aab2fd912efb0806cab7c2
Emmanuel Okoro <emmaokoro18@gmail.com>
2023-07-25 00:25:02 -0500

    Updated User.sh > redirection..

--- Roboshop Project/Redis.sh
+++ Roboshop Project/Redis.sh
@@ -1,10 +1,15 @@
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[34m>>>>>>>>>>> Install Redis Repo <<<<<<<<<<<<\e[0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
 
-yum module enable redis:remi-6.2 -y
+echo -e "\e[34m>>>>>>>>>>> Create Redis Dependencies <<<<<<<<<<<<\e[0m"
+yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 
-yum install redis -y
+echo -e "\e[34m>>>>>>>>>>> Install Redis <<<<<<<<<<<<\e[0m"
+yum install redis -y &>>/tmp/roboshop.log
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+echo -e "\e[34m>>>>>>>>>>> Replace IP Redis.conf <<<<<<<<<<<<\e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 
-systemctl enable redis
-systemctl restart redis
+echo -e "\e[34m>>>>>>>>>>> Start Redis <<<<<<<<<<<<\e[0m"
+systemctl enable redis &>>/tmp/roboshop.log
+systemctl restart redis &>>/tmp/roboshop.log
--- Roboshop Project/User.sh
+++ Roboshop Project/User.sh
@@ -1,26 +1,43 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[34m>>>>>>>>>>> Install NodeJS repo <<<<<<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-yum install nodejs -y
+echo -e "\e[34m>>>>>>>>>>> Install NodeJS <<<<<<<<<<<<\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
 
-useradd roboshop
-mkdir /app
+echo -e "\e[34m>>>>>>>>>>> Create App User <<<<<<<<<<<<\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+echo -e "\e[34m>>>>>>>>>>> Delete existing Service App <<<<<<<<<<<<\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+
+echo -e "\e[34m>>>>>>>>>>> Create Service App <<<<<<<<<<<<\e[0m"
+mkdir /app &>>/tmp/roboshop.log
+
+echo -e "\e[34m>>>>>>>>>>> Download App Content <<<<<<<<<<<<\e[0m"
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+
+echo -e "\e[34m>>>>>>>>>>> Extract App Content <<<<<<<<<<<<\e[0m"
 cd /app
-unzip /tmp/user.zip
+unzip /tmp/user.zip &>>/tmp/roboshop.log
 
+echo -e "\e[34m>>>>>>>>>>> Install NOdeJS Dependencies <<<<<<<<<<<<\e[0m"
 cd /app
-npm install
+npm install &>>/tmp/roboshop.log
 
-cp User.service /etc/systemd/system/user.service
+echo -e "\e[34m>>>>>>>>>>> Create User System service <<<<<<<<<<<<\e[0m"
+cp User.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
 
-systemctl daemon-reload
 
-cp Mongo.service /etc/yum.repos.d/mongo.repo
+echo -e "\e[34m>>>>>>>>>>> Install Mongo Repo <<<<<<<<<<<<\e[0m"
+cp Mongo.service /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-yum install mongodb-org-shell -y
+echo -e "\e[34m>>>>>>>>>>> Install MongoDB client <<<<<<<<<<<<\e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
-mongo --host mongodb.bigetech.online </app/schema/user.js
+echo -e "\e[34m>>>>>>>>>>> Load Schema <<<<<<<<<<<<\e[0m"
+mongo --host mongodb.bigetech.online </app/schema/user.js &>>/tmp/roboshop.log
 
-systemctl enable user
-systemctl restart user
+echo -e "\e[34m>>>>>>>>>>> Start User <<<<<<<<<<<<\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable user &>>/tmp/roboshop.log
+systemctl restart user &>>/tmp/roboshop.log
