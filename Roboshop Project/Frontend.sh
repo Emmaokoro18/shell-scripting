a8b7852044adb7b5d3b12c9dd1b2732787f7aeb0
Emmanuel Okoro <emmaokoro18@gmail.com>
2023-07-24 22:54:11 -0500

    Updated Frontend.sh > color.

--- Roboshop Project/Frontend.sh
+++ Roboshop Project/Frontend.sh
@@ -1,13 +1,21 @@
+echo -e "\e[34m>>>>>>>>>>> Install NGINX <<<<<<<<<<<<\e[0m"
 yum install nginx -y
+
+echo -e "\e[34m>>>>>>>>>>> Create Frontend Services <<<<<<<<<<<<\e[0m"
 cp Frontend.service /etc/nginx/default.d/roboshop.conf
 
+echo -e "\e[34m>>>>>>>>>>> Delete existing Nginx content <<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[34m>>>>>>>>>>> Download Application Content <<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[34m>>>>>>>>>>> Extract App content <<<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 
-
+echo -e "\e[34m>>>>>>>>>>> Start Frontend <<<<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl restart nginx
 
