1950dc93df97edb5164a2e7e28a05d569f074a3f
Emmanuel Okoro <emmaokoro18@gmail.com>
2023-07-24 22:30:29 -0500

    Updated Catalogue.sh > color

--- Roboshop Project/Catalogue.sh
+++ Roboshop Project/Catalogue.sh
@@ -1,38 +1,38 @@
-echo echo ">>>>>>>>>>> Create Catalogue Service <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Create Catalogue Service <<<<<<<<<<<<\e[0m"
 cp Catalogue.service /etc/systemd/system/catalogue.service
-echo ">>>>>>>>>>> Create MongoDB Repo/Service <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Create MongoDB Repo/Service <<<<<<<<<<<<\e[0m"
 cp Mongo.service /etc/yum.repos.d/mongo.repo
 
-echo ">>>>>>>>>>> Install NodeJS repos  <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Install NodeJS repos  <<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo ">>>>>>>>>>> Install NodeJS <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Install NodeJS <<<<<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo ">>>>>>>>>>> Create Application User <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Create Application User <<<<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo ">>>>>>>>>>> Make App Directory <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Make App Directory <<<<<<<<<<<<\e[0m"
 mkdir /app
 
-echo ">>>>>>>>>>> Download Application Content <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Download Application Content <<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo ">>>>>>>>>>> Unzip Application Content <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Unzip Application Content <<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 
-echo ">>>>>>>>>>> Install NodeJS Dependencies <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Install NodeJS Dependencies <<<<<<<<<<<<\e[0m"
 cd /app
 npm install
 
-echo ">>>>>>>>>>> Install Mongo client <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Install Mongo client <<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-echo ">>>>>>>>>>> Load Catalogue Schema <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Load Catalogue Schema <<<<<<<<<<<<\e[0m"
 mongo --host mongodb.bigetech.online </app/schema/catalogue.js
 
-echo ">>>>>>>>>>> Start Catalogue Service <<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>> Start Catalogue Service <<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
