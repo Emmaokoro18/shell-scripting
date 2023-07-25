f87bba67c8c179e5c643ad9ee89057ed98a8b95e
Emmanuel Okoro <emmaokoro18@gmail.com>
2023-07-25 00:09:30 -0500

    Updated MongoDB.sh > redirection..

--- Roboshop Project/MongoDB.sh
+++ Roboshop Project/MongoDB.sh
@@ -1,12 +1,12 @@
 echo -e "\e[34m>>>>>>>>>>> Install MongoDB Repo <<<<<<<<<<<<\e[0m"
-cp Mongo.service /etc/yum.repos.d/mongo.repo &>/tmp/roboshop.log
+cp Mongo.service /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[34m>>>>>>>>>>> Install MongoDB <<<<<<<<<<<<\e[0m"
-yum install mongodb-org -y &>/tmp/roboshop.log
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[34m>>>>>>>>>>> Replace mongo.conf <<<<<<<<<<<<\e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
 
 echo -e "\e[34m>>>>>>>>>>> Start MongoDB <<<<<<<<<<<<\e[0m"
-systemctl enable mongod &>/tmp/roboshop.log
-systemctl restart mongod &>/tmp/roboshop.log
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
