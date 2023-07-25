25511a65f9ceb92ad631226ff6d59bd86ca0f506
Emmanuel Okoro <emmaokoro18@gmail.com>
2023-07-25 01:00:20 -0500

    Updated Mysql.sh > redirection...

--- Roboshop Project/Mysql.sh
+++ Roboshop Project/Mysql.sh
@@ -1,13 +1,19 @@
-yum module disable mysql -y
+echo -e "\e[34m>>>>>>>>>>> Disable existing Mysql <<<<<<<<<<<<\e[0m"
+yum module disable mysql -y &>>/tmp/roboshop.log
 
-cp Mysql.service /etc/yum.repos.d/mysql.repo
+echo -e "\e[34m>>>>>>>>>>> Install Mysql repo <<<<<<<<<<<<\e[0m"
+cp Mysql.service /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
-yum install mysql-community-server -y
+echo -e "\e[34m>>>>>>>>>>> Install Mysql client <<<<<<<<<<<<\e[0m"
+yum install mysql-community-server -y &>>/tmp/roboshop.log
 
+echo -e "\e[34m>>>>>>>>>>> Create Password <<<<<<<<<<<<\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
 
-mysql_secure_installation --set-root-pass RoboShop@1
+echo -e "\e[34m>>>>>>>>>>> Set User App <<<<<<<<<<<<\e[0m"
+mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
 
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "\e[34m>>>>>>>>>>> Start Mysql <<<<<<<<<<<<\e[0m"
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl start mysqld &>>/tmp/roboshop.log
 
-mysql -uroot -pRoboShop@1
